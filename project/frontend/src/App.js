3f64990430a0b1bd6bee3204bb95088e5b87a9d0
narsss1234 <narsss1234@gmail.com>
2025-02-03 17:40:20 +0530

    side drawer is also added

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
+      <MainNavigation />
+      <main>
       <Routes>
         <Route path="/" element={<Users />} exact />
         <Route path="/places/new" element={<NewPlace />} exact />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+      </main>
     </Router>
   );
 };
--- project/frontend/src/user/pages/Users.js
+++ project/frontend/src/user/pages/Users.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import UsersList from "../components/UsersList";
 
-import "./Users.css";
-
 const Users = () => {
     const USERS = [
         {id: 'u1', name: 'Max', image: 'https://play-lh.googleusercontent.com/vco-LT_M58j9DIAxlS1Cv9uvzbRhB6cYIZJS7ocZksWRqoEPat_QXb6fVFi77lciJZQ=w526-h296-rw', palces: '3'}
