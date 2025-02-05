2a2f3199f5583d4363dd48e47b60e59682aa9356
narsss1234 <narsss1234@gmail.com>
2025-02-05 13:33:50 +0530

    taking a pause for frontend

--- project/frontend/src/places/pages/NewPlace.js
+++ project/frontend/src/places/pages/NewPlace.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import { VALIDATOR_REQUIRE } from '../../shared/components/util/validators';
+import './NewPlace.css';
+import Input from '../../shared/components/FormElements/Input';
+
 const NewPlace = () => {
     return (
-        <h2>New places - Work in Progress!</h2>
+        <form className="palce-form">
+            <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid Text" />
+        </form>
     );
 };
 
---
+++ project/frontend/src/shared/components/UIElements/Map.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Map.css';
+
+const Map = () => {
+    return (
+        <div className={`map ${PaymentResponse.className}`} style={props.style}>
+            <h1>DUMMY_MAP</h1>
+        </div>
+    );
+};
+
+export default Map;
